5e576df14613c4981a7f1b73ce790a040409bd1e
BobinYang <xiang-ai-2002@163.com>
2020-07-10 14:17:51 +0800

    初始化

---
+++ Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.panel1 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(960, 52);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "下载";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(1358, 25);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "https://docs.microsoft.com/zh-cn/aspnet/mvc/overview/getting-started/getting-star" +
+    "ted-with-ef-using-mvc/implementing-inheritance-with-the-entity-framework-in-an-a" +
+    "sp-net-mvc-application";
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Location = new System.Drawing.Point(1181, 52);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(168, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "代码复制到剪贴板";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser1.Location = new System.Drawing.Point(0, 0);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(1395, 344);
+            this.webBrowser1.TabIndex = 4;
+            this.webBrowser1.Resize += new System.EventHandler(this.webBrowser1_Resize);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.Location = new System.Drawing.Point(0, 0);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(1395, 324);
+            this.richTextBox1.TabIndex = 5;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.splitContainer1.Location = new System.Drawing.Point(0, 88);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.richTextBox1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.webBrowser1);
+            this.splitContainer1.Size = new System.Drawing.Size(1395, 672);
+            this.splitContainer1.SplitterDistance = 324;
+            this.splitContainer1.TabIndex = 6;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1395, 82);
+            this.panel1.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1395, 760);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,149 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    [ComVisible(true)]
+    public partial class Form1 : Form
+    {
+        private string content = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.webBrowser1.Url = new System.Uri(Application.StartupPath + "\\kindeditor\\e.html", System.UriKind.Absolute);
+            this.webBrowser1.ObjectForScripting = this;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.richTextBox1.Clear();
+            //网页地址：
+            string Url = this.textBox1.Text.Trim();
+
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Ssl3 | SecurityProtocolType.Tls | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls12;
+
+            List<string> list = new List<string>(); ;
+            HtmlWeb htmlWeb = new HtmlWeb();
+            htmlWeb.OverrideEncoding = Encoding.UTF8;
+
+            HtmlAgilityPack.HtmlDocument htmlDoc = htmlWeb.Load(Url);
+
+            HtmlNode node = htmlDoc.DocumentNode.SelectSingleNode("//main[@id='main']");
+
+            //去掉英文翻译
+            var a = node.SelectNodes("//span[@class='sxs-lookup']");
+            foreach (HtmlNode b in a)
+
+            {
+                b.Remove();
+            }
+
+            string src = "";
+            //图片相对路径改成绝对路径
+            var imgNode = node.SelectNodes("//img[@data-linktype='relative-path']");
+            foreach (HtmlNode node1 in imgNode)
+            {
+                src = node1.GetAttributeValue("src", "");
+                var url = new Uri(htmlWeb.ResponseUri, src);
+                node1.SetAttributeValue("src", url.AbsoluteUri);
+            }
+
+            //链接路径转换
+            var hrefNode = node.SelectNodes("//a[@data-linktype='relative-path']|//a[@data-linktype='absolute-path']");
+            foreach (HtmlNode node1 in hrefNode)
+            {
+                src = node1.GetAttributeValue("href", "");
+                var url = new Uri(htmlWeb.ResponseUri, src);
+                node1.SetAttributeValue("href", url.AbsoluteUri);
+            }
+
+            //找到所有的H2标签，然后加上顺序。
+            var h2Node = node.SelectNodes("//h2");
+            var arr = new string[] { "一", "二", "三", "四", "五", "六", "七", "八", "九", "十", "十一", "十二", "十三", "十四", "十五", "十六", "十七", "十八", "十九", "二十" };
+            if (h2Node != null)
+            {
+                for (int i = 0; i < h2Node.Count; i++)
+                {
+                    h2Node[i].InnerHtml = arr[i] + "、" + h2Node[i].InnerHtml;
+                    //找到所有的H3标签，然后加上顺序。
+
+                    var h3Node = h2Node[i].SelectNodes("following-sibling::h2|following-sibling::h3");
+                    if (h3Node is null)
+                        break;
+                    for (int j = 0; j < h3Node.Count; j++)
+                    {
+                        if (h3Node[j].Name == "h2")
+                            break;
+                        else
+                            h3Node[j].InnerHtml = (j + 1) + "、" + h3Node[j].InnerHtml;
+                    }
+                }
+            }
+            HtmlNode myNOde = htmlDoc.CreateElement("div");
+
+            //去掉前面无用的部分
+            var OK = node.SelectNodes("nav[1]/following-sibling::*");
+            myNOde.AppendChildren(OK);
+
+            //添加原文连接：
+            HtmlNode nodeOriUrl = htmlDoc.CreateElement("p");
+            nodeOriUrl.InnerHtml = "原文：<a href='" + htmlWeb.ResponseUri + "'>" + htmlWeb.ResponseUri + "</a>";
+            myNOde.PrependChild(nodeOriUrl);
+
+            ////写入到本地文件
+            //TextWriter wr = new StreamWriter(@"aa.html");
+            //myNOde.WriteTo(wr);
+            //wr.Close();
+            richTextBox1.Focus();
+            this.richTextBox1.Text = myNOde.OuterHtml;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.richTextBox1.SelectAll();//全选文本框种的文本
+            this.richTextBox1.Copy();//把选中的文本复制大剪切板
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.textBox1.Focus();
+            this.textBox1.SelectAll();
+        }
+
+        public void SetDetailContent()
+        {
+            webBrowser1.Document.InvokeScript("setContent", new object[] { content });
+        }
+
+        public string GetContent()
+        {
+            return content;
+        }
+
+        public void RequestContent(string str)
+        {
+            content = str;
+            richTextBox1.Text = content;
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (richTextBox1.Focused)
+            {
+                content = richTextBox1.Text;
+                SetDetailContent();
+            }
+        }
+
+        private void webBrowser1_Resize(object sender, EventArgs e)
+        {
+            this.webBrowser1.Refresh();
+        }
+    }
+}
