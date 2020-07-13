0673105ff56d3633bdb73aa0c8af9669662aee8f
BobinYang <xiang-ai-2002@163.com>
2020-07-13 08:36:28 +0800

    更改焦点

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -48,7 +48,7 @@
             this.button1.Location = new System.Drawing.Point(960, 52);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 2;
+            this.button1.TabIndex = 1;
             this.button1.Text = "下载";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
@@ -60,7 +60,7 @@
             this.textBox1.Location = new System.Drawing.Point(12, 12);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(1358, 25);
-            this.textBox1.TabIndex = 1;
+            this.textBox1.TabIndex = 0;
             this.textBox1.Text = "https://docs.microsoft.com/zh-cn/aspnet/mvc/overview/getting-started/getting-star" +
     "ted-with-ef-using-mvc/implementing-inheritance-with-the-entity-framework-in-an-a" +
     "sp-net-mvc-application";
@@ -71,7 +71,7 @@
             this.button2.Location = new System.Drawing.Point(1181, 52);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(168, 23);
-            this.button2.TabIndex = 3;
+            this.button2.TabIndex = 2;
             this.button2.Text = "代码复制到剪贴板";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
@@ -83,7 +83,7 @@
             this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
             this.webBrowser1.Name = "webBrowser1";
             this.webBrowser1.Size = new System.Drawing.Size(1395, 344);
-            this.webBrowser1.TabIndex = 4;
+            this.webBrowser1.TabIndex = 0;
             this.webBrowser1.Resize += new System.EventHandler(this.webBrowser1_Resize);
             // 
             // richTextBox1
@@ -92,7 +92,7 @@
             this.richTextBox1.Location = new System.Drawing.Point(0, 0);
             this.richTextBox1.Name = "richTextBox1";
             this.richTextBox1.Size = new System.Drawing.Size(1395, 324);
-            this.richTextBox1.TabIndex = 5;
+            this.richTextBox1.TabIndex = 0;
             this.richTextBox1.Text = "";
             this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
             // 
@@ -125,7 +125,7 @@
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(1395, 82);
-            this.panel1.TabIndex = 7;
+            this.panel1.TabIndex = 0;
             // 
             // Form1
             // 
